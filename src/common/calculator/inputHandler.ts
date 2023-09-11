2ba1efc215b869445c15cd25ce27477495b91507
cdlin <cdlin@lacewingtech.com>
2023-09-11 18:26:31 +0800

    feat: update usd exchange

--- src/common/calculator/inputHandler.ts
+++ src/common/calculator/inputHandler.ts
@@ -5,7 +5,7 @@ export class InputHandler {
     return source
       .replace(/,/g, '')
       .replaceAll('jpy', '*0.22')
-      .replaceAll('usd', '*31')
+      .replaceAll('usd', '*32')
       .replaceAll('rmb', '*4.37')
       .replaceAll('vnd', '*0.0013')
       .replaceAll('thb', '*0.9')
